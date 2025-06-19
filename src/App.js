97fd09aaba1b419dbade1e75b217a64ae934cc0c
Rathod-1122 <kethavathanil17@gmail.com>
2025-06-19 22:26:20 +0530

    created h1 in local repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>It is Local copy of the Remote Repository</h1>
+      
+    </div>
+  );
+}
+
+export default App;
