ac81e2652f459782c38163f8d85421527db1c69d
Rathod-1122 <kethavathanil17@gmail.com>
2025-06-20 11:54:15 +0530

    added h2

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>It is Local copy of the Remote Repository</h1>
+      <h2>Second heading</h2>
       
     </div>
   );
 }
 
-export default App;
+export default App;      
